9ee44b5fbb3811140f6810d30186aa569ed600c0
Eduardo Santos <4b4c4t3666@gmail.com>
2025-09-01 22:03:50 -0300

    a

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -77,7 +77,7 @@ export function AppSidebar() {
     const { auth } = usePage().props as any;
 
     if (auth?.user?.role !== 'admin') {
-        mainNavItems[1].items.pop();
+        mainNavItems[1].pop();
     }
 
     return (
